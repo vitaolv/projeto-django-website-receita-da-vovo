d7e170751ca4a9636bb5c66edd783fd730b1d380
vitaolv <vlveleda@gmail.com>
2023-02-24 20:07:09 -0300

    Atualização do projeto e JsonResponse adicionado.

--- recipes/urls.py
+++ recipes/urls.py
@@ -25,4 +25,16 @@ urlpatterns = [
         views.RecipeListViewSearch.as_view(),
         name="search"
     ),
+
+    path(
+        'recipes/api/v1/',
+        views.RecipeListViewHomeApi.as_view(),
+        name="recipes_api_v1",
+    ),
+
+    path(
+        'recipes/api/v1/<int:pk>/',
+        views.RecipeDetailAPI.as_view(),
+        name="recipes_api_v1_detail",
+    ),
 ]
